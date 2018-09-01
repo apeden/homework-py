57b6eacda693b8d7cb63cfc5eee18d5643d45006
apeden <apeden23@gmail.com>
2018-09-01 23:30:48 +0100

    tidied up variable names in Homework class

--- generic homework.py
+++ generic homework.py
@@ -3,57 +3,104 @@ import random
 class Homework():
     def __init__(self):
         """
-        Instantiates a homework object with attribute of name and user input and task
+        Instantiates a homework object with attributes of the actual answer to the question\
+        and the users guess. Until these are set by a setter function they remain as
+        NoneType
         """
-        self.name = None
-        self.task = None
-        self.ans = None
-        self.user_input = None
+        self.actual_ans = None
+        self.user_guess = None
         
         
     def check(self):
         """
         checks answer given by user, return True or False
         """        
-        return self.user_input == self.ans
+        return self.user_guess == self.actual_ans
 
 
-    def answer(self):
+    def user_guess(self):
         """
-        Asks for user input to question and gives printed response.\
+        Asks for user input to in reponse to a question
         Returns user input
         """
         pass
+    
 
-    def openfile(self):
 
-
-class Homework():
+class Addition(Homework):
+    task = 'Addition' ##class variable
     def __init__(self):
         """
-        Instantiates a Sum object with two random ints\
+        Instantiates an Addition (Homework) object with two random ints\
         in a range
         """
+        Homework.__init__(self)
         self.x = random.randint(1,12)
         self.y = random.randint(1,12)
-        self.ans = None
-        self.user_input = None
-        self.label = None
-        
-    def check(self):
-        """
-        checks answer given by user, return True or False
-        """        
-        return int(self.user_input) == self.ans
 
-    def add(self):
+
+    def getAddAns(self):
         """
         return correct answer
         """ 
-        self.label = "Addition practice"
         self.ans = self.x + self.y
-        print('in add self ans is',self.ans)
         return self.ans
+
+
+    def getUserInput(self):
+        """
+        Asks for user input to in reponse to a question
+        Returns user input as int
+        """
+        string_input = input('What is'+self.x+'+'+self.y+'?:')
+        self.user_input = int(string_input)        
+        return self.user_input
+
+
+
+
+
+
+
+
+
+
+class Session():
+    def __init__(self, task, num_questions):
+        """
+        instantiates a homework session
+        task: int used as an index to select a task from a dict inside this class
+        num_questions = int indicating number of questions to be asked
+        """
+        self.task = task_dict[task]
+        self.num_questions = num_questions
+        self.name = None
+        self.date = None        
+
+    def ():
+        for i in range(num_questions):
+            
+            print('Question', i,'of ', self.num_qestions)
+        
+    def setDate(self):
+        """
+        Asks user to input date 
+        Returns user input (string)
+        """
+        self.date = input("What is today's date?:")
+
+    def setName(self):
+        """
+        Asks user to input name 
+        Returns user input (string)
+        """
+        self.date = input("What is your name?:")
+
+
+
+
+
+
 
     def answer(self):
         """Asks for user input to question and gives printed response. Return nothing """
@@ -66,24 +113,6 @@ class Homework():
             self.check()
         else:
             print('Correct, well done!')
-
-
-
-
-
-
-
-
-class Worktime():
-    def __init__(self, question_class, num_questions):
-        self.num_questions = num_questions
-        self.question_class = question_class
-
-    def session():
-        for i in range(num_questions):
-            
-            print('Question', i,'of ', self.num_qestions)
-        
                        
 
 
