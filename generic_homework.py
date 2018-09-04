70b9fc793c4a75ac99d1ac3b4968b3ba59f3e205
apeden <apeden23@gmail.com>
2018-09-05 00:59:16 +0100

    Expanded the Session class. Unit testing the question_check and it's going OK, but it does not display the question to the user

--- generic_homework.py
+++ generic_homework.py
@@ -95,7 +95,8 @@ class Addition(Homework):
         with the actual answer
         This method is not activated in the Homework parent class  
         """
-        self.user_guess = int(input())
+        user_ans = input(':')
+        self.user_guess = int(user_ans)
         assert type(self.user_guess) == int
         
     def getXY(self):
@@ -105,7 +106,7 @@ class Addition(Homework):
         return (self.x,self.y)
 
 class Session():
-    def __init__(self, num_questions, homework_task):
+    def __init__(self, student_name, num_questions, homework_task):
         """
         instantiates a homework session object using number of questions
         and a home_work task as inputs
@@ -114,23 +115,35 @@ class Session():
         homework_task: the class of homework to be used to generate 
         objects. 
         """
+        self.student_name = student_name
+        assert type(self.student_name) == str
         self.num_questions = num_questions
+        assert type(self.num_questions) == int
         self.homework_task = homework_task
         self.record_file = open("homework.txt", "w")
     
     def question_check(self):        
-        self.homework_task.ask_question()
-        self.homework_task.setUser_guess()
-        self.homework_task.setActual_ans()
-            if self.check():
-                print("We done
+        q= self.homework_task()
+        q.setActual_ans()
+        attempt = 1
+        while True:            
+            q.ask_questions()
+            q.setUser_guess()
+            if q.check():
+                print("Well done,",self.student_name," , that's right")
+                ##record to file
+                self.record_file.write(self.student_name + "gave the \
+                                       right answer"
+                + str(attempt) + "attempt(s)")
+                del q
+                break
+            print("Try again,",self.student_name)
+            attempt += 1
     
-    
-    
-    def run_task:
+    def run_task(self):
         question = 1
-        while question < self.num_questions+1:
-            self.homework_task.setUser_input()
-            self.homework_task.setUser_guess()
-            self.homework_task.setActual_ans()
-            if self.get
+        while question < self.num_questions + 1:
+            self.record_file.write("Question "+str(question))
+            question_check()
+            question += 1
+        self.record_file.close()
--- generic_homework_test_harness.py
+++ generic_homework_test_harness.py
@@ -7,7 +7,7 @@ Created on Sun Sep  2 08:16:42 2018
 
 import random
 from generic_homework import *
-
+###TESTING OF THE HOMEWORK CLASS####
 #h = Homework()
 #print(h.check())
 #print(h.setUser_input())
@@ -17,14 +17,25 @@ from generic_homework import *
 #print(h.getUser_input())
 #print(h.getUser_guess())
 #
-j = Addition()
 
-print(j.ask_questions())
-print(j.setUser_guess())
-print(j.setActual_ans())
-print(j.getActual_ans())
+####TESTING OF THE ADDITION CLASS####
+j = Addition()
+r = 3
+print(type(j))
+print(type(r))
+#print(j.ask_questions())
+#print(j.setUser_guess())
+#print(j.setActual_ans())
+#print(j.getActual_ans())
 #print(j.getUser_input())
 #print(j.getUser_guess())
 #print(j.getXY())
-print(j.check())
+#print(j.check())
 #print(j.task)
+
+####TESTING OF SESSION CLASS####
+work = Session("Euan",4,Addition)
+#print(work.student_name)
+#print(work.num_questions)
+#print(work.homework_task)
+work.question_check()
