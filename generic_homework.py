333c62eb7cc1462000f1516facee607e4f7f7f00
PEDEN Alexander <apeden@ed.ac.uk>
2018-09-04 14:00:41 +0100

    Modified def question_check in class Session

--- generic_homework.py
+++ generic_homework.py
@@ -104,33 +104,33 @@ class Addition(Homework):
         """
         return (self.x,self.y)
 
-#class Session():
-#    def __init__(self, num_questions, homework_task):
-#        """
-#        instantiates a homework session object using number of questions
-#        and a home_work task as inputs
-#        
-#        num_questions: int indicating number of questions to be asked
-#        homework_task: the class of homework to be used to generate 
-#        objects. 
-#        """
-#        self.num_questions = num_questions
-#        self.homework_task = homework_task
-#        self.record_file = open("homework.txt", "w")
-#    
-#    def question_check(self):
-#        self.homework_task.setUser_input()
-#        self.homework_task.setUser_guess()
-#        
-#        self.homework_task.setActual_ans()
-#            if self.get
-#    
-#    
-#    
-#    def run_task:
-#        question = 1
-#        while question < self.num_questions+1:
-#            self.homework_task.setUser_input()
-#            self.homework_task.setUser_guess()
-#            self.homework_task.setActual_ans()
-#            if self.get
+class Session():
+    def __init__(self, num_questions, homework_task):
+        """
+        instantiates a homework session object using number of questions
+        and a home_work task as inputs
+        
+        num_questions: int indicating number of questions to be asked
+        homework_task: the class of homework to be used to generate 
+        objects. 
+        """
+        self.num_questions = num_questions
+        self.homework_task = homework_task
+        self.record_file = open("homework.txt", "w")
+    
+    def question_check(self):        
+        self.homework_task.ask_question()
+        self.homework_task.setUser_guess()
+        self.homework_task.setActual_ans()
+            if self.check():
+                print("We done
+    
+    
+    
+    def run_task:
+        question = 1
+        while question < self.num_questions+1:
+            self.homework_task.setUser_input()
+            self.homework_task.setUser_guess()
+            self.homework_task.setActual_ans()
+            if self.get
